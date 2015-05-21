6374cf17c052f44bca096d68b9f579a5f8b15a59
Ryan Rishi <rdrishi12@gmail.com>
2015-05-21 14:41:31 -0700

    general cleanup of repo

--- assignments/expedia/exploration.r
+++
@@ -1,9 +0,0 @@
-# this file contains my notes. Said notes may not be coherent.
-
-table(train$prop_starrating)  # 
-table(train$visitor_hist_starrating)  # this won't to much good since about 285k / 300k of these values are 0
-table(train$visitor_hist_adr_usd)     # "". Darn you privacy!
-
-# for testing, do something like this:
-table(booking_bool == train$booking_bool) # the more TRUES, the better
-
--- assignments/expedia/test.r
+++
@@ -1,8 +0,0 @@
-# Get random subset. For testing purposes only.
-sub <-
-train <- sub
-
-
-boxplot(train)
-
-
